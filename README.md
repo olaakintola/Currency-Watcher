fa8896eb8048f49f1b7582654e61ef49d732b703
olaakintola <olanipekun_akin@yahoo.co.uk>
2020-05-03 17:36:41 +0100

    new update with complete project

--- README.md
+++
@@ -1,7 +0,0 @@
-# Currency-Watcher
-
-System will watch current values of cryptocurrencies
-
-It will allow users to create notifications
-
-This project will be written mostly in Java.
---
+++ src/main/java/currencywatcher/CurrencyWatcher/CurrencyWatcherApplication.java
@@ -0,0 +1,14 @@
+package currencywatcher.CurrencyWatcher;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyWatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyWatcherApplication.class, args);
+	}
+
+}
